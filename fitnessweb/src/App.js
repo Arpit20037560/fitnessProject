3b3c54a2690d61023786d2d82f69a2e22cd63127
Arpit Beuria <arpitbeuria196@gmail.com>
2024-11-16 08:35:40 +0000

    Updated App include Provider and Router Provieder

--- fitnessweb/src/App.js
+++ fitnessweb/src/App.js
@@ -1,9 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
+import store from "./features/appStore";
 
 function App() {
+
+  const appRouter = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Login />
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      },
+    ]
+  );
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Provider store={store}> {/* Wrap the RouterProvider inside the Provider */}
+      <RouterProvider router={appRouter} />
+    </Provider>
   );
 }
 
