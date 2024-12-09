d9a1c87c6cb7970ff8705ebcfd6b2e1aaaa5accf
Arpit Beuria <arpitbeuria196@gmail.com>
2024-12-09 00:41:07 +0000

    bcryptjs

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const authRouter = express.Router();
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const validateData = require("../utils/validate");
 const User = require("../models/User");
 
